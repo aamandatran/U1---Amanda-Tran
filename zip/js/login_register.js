31dad2d4c93a6e8c9e9020c9f17ca3966caf1386
tranenn <aamandatran@gmail.com>
2023-02-13 19:56:51 +0100

    Added CSS transition-property and duration for smoother transition between register and login

--- zip/js/login_register.js
+++ zip/js/login_register.js
@@ -7,11 +7,13 @@ function login_register() {
     document.querySelector("#portal_register").addEventListener("click", e => {
         login.classList.add("hidden");
         register.classList.remove("hidden");
+        document.getElementById("wrapper").style.backgroundColor = "rgb(110, 134, 110)";
     });
 
     document.querySelector("#portal_login").addEventListener("click", e => {
         register.classList.add("hidden");
         login.classList.remove("hidden");
+        document.getElementById("wrapper").style.backgroundColor = "rgb(154, 182, 154)";
     });
 }
 
